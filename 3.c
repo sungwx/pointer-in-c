907e04bd5c304abd3b9ebfeb9c31ecf68438dfca
sungwx <sungwx@qq.com>
2018-10-03 07:30:07 +0000

    update

---
+++ 3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(int* a,int* b)
+{
+    int* t = a;
+    a = b;
+    b = t;
+}
+
+int main()
+{
+    int* a ,* b;
+    a = (int*)malloc(sizeof(int));
+    b = (int*)malloc(sizeof(int));
+    printf("init adrees is %d %d\n",(int)(a),(int)(b));
+    (*a) = 1;
+    (*b) = 2;
+    printf("after adrees is %d %d\n",(int)(a),(int)(b));
+    // int* t = a;
+    // a = b;
+    // b = t;
+    if(swap(a,b))
+    {
+        printf("after adrees is %d %d\n",(int)(a),(int)(b));
+        printf("value is %d %d\n",*a,*b);
+    }
+
+    return 0;
+}
